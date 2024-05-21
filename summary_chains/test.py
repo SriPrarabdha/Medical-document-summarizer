3a70cde2f7851c7e8812d1e8cc6646469c7d2f18
Krishan Walia <krishanw30@gmail.com>
2024-05-21 21:48:59 +0530

    Updated test.py

--- summary_chains/test.py
+++ summary_chains/test.py
@@ -48,8 +48,8 @@ texts_split = text_splitter.split_text(text)
 docs = [Document(page_content=text) for text in texts_split]
 
 response = map_reduce_summary(docs, model)
-# response = refine_summarize(docs, model) -> Working
-# response = raptor_summarize(model, docs) -> Generating summary with a Bad Argument Error
+# response = refine_summarize(docs, model) #-> Working
+# response = raptor_summarize(model, docs) #-> Generating summary with a Bad Argument Error
 
 print(response)
 
