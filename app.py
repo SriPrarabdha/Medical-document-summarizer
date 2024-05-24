26512b016dc694bbebf594a8fc4b56e30db05fb4
Anupam Maurya <anupammaurya6767@gmail.com>
2024-05-24 14:35:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import asyncio
+import streamlit as st
+from summary_chains.stuff_summarization import stuff_summarize
+from summary_chains.refine_summarization import refine_summarize
+from summary_chains.raptor_summarization import raptor_summarize
+from summary_chains.map_reduce_summarization import map_reduce_summary
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import PromptTemplate
+from langchain.docstore.document import Document
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
+import json
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from report_summary_extractor import report_summary_extractor
+from report_wise_json_segregator import report_wise_json_data_extraction
+
 def get_or_create_eventloop():
     try:
         return asyncio.get_event_loop()
@@ -10,70 +27,54 @@ def get_or_create_eventloop():
 
 get_or_create_eventloop()
 
-import streamlit as st
-from summary_chains.stuff_summarization import stuff_summarize
-from summary_chains.refine_summarization import refine_summarize
-from summary_chains.raptor_summarization import raptor_summarize
-from summary_chains.map_reduce_summarization import map_reduce_summary
-
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
-from langchain.docstore.document import Document
-
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
-from langchain_core.messages import HumanMessage
-import google.generativeai as genai
-import json
-
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 st.title("LLM Assistant")
 input_prompt = st.text_area("Enter your prompt")
 
 data = st.selectbox("Choose a pdf", ["Medical_Record_File_1", "Medical_Record_File_2", "Medical_Record_File_3"])
-method = st.selectbox("Choose a method", ["Stuff", "Map_Reduce", "Refine", "Raptor"])
+method = st.selectbox("Choose a method", ["Stuff", "Map_Reduce", "Refine", "Raptor", "Summary"])
 chunk_size = st.slider("Chunk Size", 1000, 5000, value=4000, step=100)
 
-# os.environ["GOOGLE_API_KEY"] = "AIzaSyCP1kveVOTOIMyzvEY6Xdwpq18567ETBPU"
 os.environ["GOOGLE_API_KEY"] = "AIzaSyDx7cfKeqr0YK0TE8767lnMz6G5NmeXJBI"
-
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-
 model = ChatGoogleGenerativeAI(model='models/gemini-1.5-pro-latest', temperature=0)
 
+prompt_template = """Given this medical report of a patient give me a concise summary for all this document in multiple points where each points follows a template like
+*Type of form/Diagnosis/examination or pharmacy/medical prescription reports/follow-up evaluations for a patient* dated *date of that form/Diagnosis/examination or pharmacy/medical prescription reports/follow-up evaluations* by *name of physician /attorney if present* - Impression : *any impression/inference from form/Diagnosis/examination or pharmacy/medical prescription reports/follow-up evaluations*
+    
+Context:
+"{text}"
+"""
+prompt = PromptTemplate.from_template(prompt_template)
+llm_chain = LLMChain(llm=model, prompt=prompt)
 
 def get_output(filename, chunk_size, method):
-    with open(f'Extracted_Data/{filename}.json', 'r') as file:
-        data = json.load(file)
-
-    text = ""
-    # print(data)
-    for key in data:
-        # print(key)
-        text= text + " " + data[key]
-        # print(data[key])
-
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=chunk_size, chunk_overlap=80
-    )
-    texts_split = text_splitter.split_text(text)
-
-    docs = [Document(page_content=text) for text in texts_split]
-
-    if method == "Stuff":
-        return stuff_summarize(docs, model)
-    elif method == "Map_Reduce":
-        return map_reduce_summary(docs, model)
-    elif method == "Refine":
-        return refine_summarize(docs, model)
-    elif method == "Raptor":
-        return raptor_summarize(docs)
+    if method == "Summary":
+        return report_summary_extractor(filename)
+    else:
+        with open(f'Extracted_Data/{filename}.json', 'r') as file:
+            data = json.load(file)
+        text = ""
+        for key in data:
+            text += " " + data[key]
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=chunk_size, chunk_overlap=80
+        )
+        texts_split = text_splitter.split_text(text)
+        docs = [Document(page_content=text) for text in texts_split]
+        if method == "Stuff":
+            return stuff_summarize(docs, model)
+        elif method == "Map_Reduce":
+            return map_reduce_summary(docs, model)
+        elif method == "Refine":
+            return refine_summarize(docs, model)
+        elif method == "Raptor":
+            return raptor_summarize(docs)
 
 if st.button("Submit"):
-    output_text = get_output(data,chunk_size,method)
-    st.markdown(output_text)
+    output_placeholder = st.empty()
+    if method == "Summary":
+        for partial_summary in get_output(data, chunk_size, method):
+            output_placeholder.markdown(partial_summary)
+    else:
+        output_text = get_output(data, chunk_size, method)
+        output_placeholder.markdown(output_text)
